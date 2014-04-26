faa7f65e5f49d89eaec3ac111eed6c9ec2f76e94
David Rusu <dave809@gmail.com>
2014-04-25 23:34:43 -0400

    Added template for new routines

---
+++ template.py
@@ -0,0 +1,14 @@
+from api import web, run, rund
+
+'''
+Tasks are run in the order they are placed in the list
+
+EXAMPLE:
+tasks = [ web('example.com')              # opens the website www.example.com in firefox
+        , run('pacman', '-Syu')           # runs and waits for the command 'pacman -Syu' to finish
+        , rund('evince', 'path/to/pdf')   # runs and doesn't wait for command to finish
+        ]
+'''
+
+tasks = [
+        ]
